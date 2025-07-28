95488a186ebec98e5795e7e6afce2a9183def404
Bernd <Bernd.Wengenroth@gmail.com>
2025-07-28 17:44:44 +0200

    Added more data types

--- src/main/java/com/bw/fsm/Data.java
+++ src/main/java/com/bw/fsm/Data.java
@@ -6,9 +6,233 @@ import java.util.Objects;
 
 public abstract class Data {
 
-    public static final Data NULL = new Null();
-
     public abstract boolean is_empty();
+
+    public static class Integer extends Data {
+        int value;
+
+        public Integer(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return false;
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return java.lang.Integer.toString(value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Integer i) {
+                return value == i.value;
+            }
+            if (o instanceof Double i) {
+                return value == i.value;
+            }
+            return false;
+        }
+
+
+    }
+
+    public static class Double extends Data {
+        double value;
+
+        public Double(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return false;
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return java.lang.Double.toString(value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Double i) {
+                return value == i.value;
+            }
+            if (o instanceof Integer i) {
+                return value == i.value;
+            }
+            return false;
+        }
+    }
+
+    public static class String extends Data {
+        java.lang.String value;
+
+        public String(java.lang.String value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return value == null || value.isEmpty();
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof String i) {
+                return Objects.equals(value, i.value);
+            }
+            return false;
+        }
+
+    }
+
+    public static class Boolean extends Data {
+        boolean value;
+
+        public Boolean(boolean value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return false;
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return value ? "true" : "false";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Boolean i) {
+                return value == i.value;
+            }
+            return false;
+        }
+    }
+
+    public static class Array extends Data {
+
+        java.util.List<Data> values;
+
+        public Array(java.util.List<Data> values) {
+            this.values = values;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return values.isEmpty();
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return values.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Array i) {
+                return values.equals(i.values);
+            }
+            return false;
+        }
+    }
+
+    public static class Map extends Data {
+
+        java.util.Map<java.lang.String, Data> values;
+
+        public Map(java.util.Map<java.lang.String, Data> values) {
+            this.values = values;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return values.isEmpty();
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return values.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Map i) {
+                return values.equals(i.values);
+            }
+            return false;
+        }
+    }
+
+    public static class Null extends Data {
+
+        private Null() {
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return "null";
+        }
+
+        @Override
+        public boolean is_empty() {
+            return true;
+        }
+
+        public static final Data NULL = new Null();
+
+    }
+
+    public static class Error extends Data {
+        java.lang.String message;
+
+        public Error(java.lang.String message) {
+            this.message = message;
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return "Error: " + message;
+        }
+
+        @Override
+        public boolean is_empty() {
+            return true;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o instanceof Error err) {
+                return Objects.equals(this.message, err.message);
+            }
+            return false;
+        }
+    }
 
     public static class Source extends Data {
         SourceCode source;
@@ -39,61 +263,23 @@ public abstract class Data {
         public boolean equals(Object o) {
             if (this == o)
                 return true;
-            if (o instanceof Source source) {
-                return Objects.equals(this.source, source.source);
+            if (o instanceof Source s) {
+                return Objects.equals(this.source, s.source);
             }
             return false;
         }
 
     }
 
-    public static class Double extends Data {
-        double value;
 
-        public Double(double value) {
-            this.value = value;
-        }
+    public static class None extends Data {
 
-        @Override
-        public boolean is_empty() {
-            return false;
-        }
-    }
-
-    public static class Integer extends Data {
-        int value;
-
-        public Integer(int value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean is_empty() {
-            return false;
-        }
-    }
-
-    public static class String extends Data {
-        java.lang.String value;
-
-        public String(java.lang.String value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean is_empty() {
-            return value == null || value.isEmpty();
-        }
-    }
-
-    public static class Null extends Data {
-
-        private Null() {
+        private None() {
         }
 
         @Override
         public java.lang.String toString() {
-            return "null";
+            return "";
         }
 
         @Override
@@ -101,5 +287,8 @@ public abstract class Data {
             return true;
         }
 
+        public static final Data NONE = new None();
     }
+
+
 }
